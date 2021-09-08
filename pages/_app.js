5ea725a360830c1dbe25d45f67f05082ef55ad89
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-09-08 14:44:04 -0700

    change to personal site/blog landing page, add spectre css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import '../node_modules/spectre.css/dist/spectre.css'
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
