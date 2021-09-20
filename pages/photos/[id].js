0242f8bf39ee3689c5a4916ee8281997508937a3
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-09-19 21:53:39 -0700

    add photos dynamic pathing update, Image in public/images now

--- pages/photos/[id].js
+++ pages/photos/[id].js
@@ -1,14 +1,24 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/photos'
 import Image from 'next/image'
+import path from 'path'
+
 
 
 export default function Post({ postData }) {
+    const postsDirectory = path.join(process.cwd(), '/public/images')
+    const fileName = postData.id + ".jpeg"
+    const fullPath = path.join(postsDirectory, fileName)
+
   return (
     <Layout>
+      {postData.title}
+      <br />
       {postData.id}
       <br />
-      <Image src={"/photos/" + postData.id + ".jpeg"} width={400} height={400} />
+      {fullPath}
+      <br />
+      <Image src={fullPath} width={144} height={144} alt={postData.id} />
       <br />
       {postData.date}
     </Layout>
