4f30e3239ae8905fbe5fd7e2a60a95b4192de3fd
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-09-20 12:32:14 -0700

    add grid photos index, clickable images to static pages

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
+import styles from '../../styles/Photos.module.css'
 import utilStyles from '../../styles/utils.module.css'
 import { getSortedPostsData } from '../../lib/photos'
 import Link from 'next/link'
 import Date from '../../components/date'
+import Image from 'next/image'
+import path from 'path'
+
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData()
@@ -15,20 +19,31 @@ export async function getStaticProps() {
   }
   
 export default function Photos({ allPostsData }) {
+    const postsDirectory = path.join(process.cwd(), '/images')
+    const fileName = allPostsData.id + ".jpg"
+    const fullPath = path.join(postsDirectory, fileName)
     return (
       <Layout Photos>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Photos</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id }) => (
-              <li className={utilStyles.listItem} key={id}>
-              <Link href={`/photos/${id}`}>
-                <a>{id}</a>
-              </Link>
-              <br />
-            </li>
-            ))}
-          </ul>
+          <article>
+            <div className="container">
+              <div className="columns">
+                <div className="column col-12">
+                  <ul className={`${utilStyles.list} ${styles.ul}`}>
+                    {allPostsData.map(({ id }) => (
+                      <li className={utilStyles.listItem} key={id}>
+                      <Link href={`/photos/${id}`}>
+                        <a><Image src={`/images/${id}.jpg`} width={283} height={283} alt={allPostsData.id} /> </a>
+                      </Link>
+                      <br />
+                    </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </article>
         </section>
       </Layout>
     )
