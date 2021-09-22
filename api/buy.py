3f5c43a4ea5d5f35b881cd97a000dd29cefca50a
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-09-22 14:35:27 -0700

    add api buy.py, Notes input redirected to buy.py

---
+++ api/buy.py
@@ -0,0 +1,15 @@
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
+
+import yfinance as yf
+import pandas as pd
+
+@app.route('/')
+@app.route('/<path:path>')
+async def post_json(request, path=""):
+    ticker = yf.Ticker(str(request.args.get("name")))
+    df = pd.DataFrame(ticker.recommendations)
+    count = pd.DataFrame(df["To Grade"].value_counts())
+    to_send = pd.DataFrame.to_json(count)
+    return json(to_send)
