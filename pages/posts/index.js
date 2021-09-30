e58ae8df87d75b99fc82dc85fffb4b3807636cc5
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-09-29 19:55:46 -0700

    convert posts index to table style mapping

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
@@ -17,22 +18,50 @@ export async function getStaticProps() {
 export default function Posts({ allPostsData }) {
     return (
       <Layout Posts>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Posts</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-            ))}
-          </ul>
-        </section>
+        <div className={utilStyles["measure-wide"]}>
+          <div >
+            <table className="table">
+              <thead className={utilStyles["black"] + " " + utilStyles["bg-light-gray"]+ " " + utilStyles["ba"] + " " + utilStyles["b--black"]}>
+                <tr>
+                  <th className={utilStyles["measure-12"]}> Date </th>
+                  <th className={utilStyles["measure-12"]}> Title</th>
+                </tr>
+              </thead>
+              {allPostsData.map(({ id, date, title }) => (
+              <tbody>
+                <tr>
+                  <td>{date}</td>
+                  <td>
+                    <Link href={`/posts/${id}`}>
+                      <a>{title}</a>
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+              ))}
+            </table>
+          </div>
+        </div>
       </Layout>
     )
   }
+        
+
+/*
+<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+  <h2 className={utilStyles.headingLg}>Posts</h2>
+  <ul className={utilStyles.list}>
+    {allPostsData.map(({ id, date, title }) => (
+      <li className={utilStyles.listItem} key={id}>
+      <Link href={`/posts/${id}`}>
+        <a>{title}</a>
+      </Link>
+      <br />
+      <small className={utilStyles.lightText}>
+        <Date dateString={date} />
+      </small>
+    </li>
+    ))}
+  </ul>
+</section>
+*/
