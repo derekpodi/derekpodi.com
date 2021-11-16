0e3ec1a694c246bb5dcb9b77ef50c39e624bf7c4
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2021-11-16 12:25:57 -0800

    lighthouse report changes

--- components/layout.js
+++ components/layout.js
@@ -10,46 +10,51 @@ export const siteTitle = 'Derek Podimatis'
 
 export default function Layout({ children, home }) {
   return (
-    <div className={utilStyles.container}>
-      
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+    <html lang="en">
 
-      <Navbar />
-
-      <Menu />
-      
-
-      <main className={utilStyles.mh3}>{children}</main>
+      <div className={utilStyles.container}>
+        
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
 
 
-      <footer className={`${utilStyles.mh3} ${utilStyles.mt4}`}>
-        {!home && (
-          <div className={utilStyles.backToHome}>
-            <Link href="/">
-              <a className={utilStyles["top-2"] + " " + utilStyles["btn"]+ " " + utilStyles["btn-sm"] + " " + utilStyles["bg-dark-green"] + " " + utilStyles["white"] + " " + utilStyles["hover-white"]+ " " + utilStyles["hover-bg-black"]}>← Back to home</a>
-            </Link>
-          </div>
-        )}
-        <div className={utilStyles.mt3}></div>
-        <Footer />
-        <div className={utilStyles["bottom-2"]}></div>
-      </footer>
+        <Navbar />
 
-    </div>
+
+        <Menu />
+        
+
+        <main className={utilStyles.mh3}>{children}</main>
+
+
+        <footer className={`${utilStyles.mh3} ${utilStyles.mt4}`}>
+          {!home && (
+            <div className={utilStyles.backToHome}>
+              <Link href="/">
+                <a className={utilStyles["top-2"] + " " + utilStyles["btn"]+ " " + utilStyles["btn-sm"] + " " + utilStyles["bg-dark-green"] + " " + utilStyles["white"] + " " + utilStyles["hover-white"]+ " " + utilStyles["hover-bg-black"]}>← Back to home</a>
+              </Link>
+            </div>
+          )}
+          <div className={utilStyles.mt3}></div>
+          <Footer />
+          <div className={utilStyles["bottom-2"]}></div>
+        </footer>
+
+      </div>
+    </html>
   )
 }
 
