379fefce99bb6be13f3f94b968d3243a6fdc4ea7
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2022-02-17 15:23:40 -0800

    Rework 404 import

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Image from 'next/image';
+import Image from 'next/image'
 
 
 export default function Custom404() {
